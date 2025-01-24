a62224ecca9a7d312f12245e0644fe9038c778f5
HirutaMint <hiruta@mintflag.com>
2025-01-25 00:50:00 +0900

    UVスクロール用のhlslファイルを追加。

---
+++ URPShaderSampleR0701/Packages/com.unity.render-pipelines.universal@14.0.10/Shaders/UVScroll.hlsl
@@ -0,0 +1,10 @@
+﻿#ifndef UV_SCROLL_INCLUDED
+#define UV_SCROLL_INCLUDED
+
+// UVスクロール実行の関数
+float2 ScrollUV(float2 uv, float2 scrollSpeed, float time)
+{
+    return uv + scrollSpeed * time;
+}
+
+#endif //UV_SCROLL_INCLUDED
